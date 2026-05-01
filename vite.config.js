d7d169f2103a008fe28b221557164cd1a7657fcd
laqlan <laqlan2180@gmail.com>
2026-05-01 11:39:14 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
-})
+  // Makes the built app work on BOTH:
+  // 1) https://username.github.io/repo-name/
+  // 2) https://outreach.puregrid.es/
+  base: './'
+});
